3b7d01647288e481618a8c5191cb7ae4c211b454
haleycatpawz <161916431+haleycatpawz@users.noreply.github.com>
2024-12-01 02:05:24 -0600

    commit 1
    first version of landing page for Catwalk Couture

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
-# Landing Page Project
+# Landing Page Catwalk Couture Project 
 
-## Instructions
 
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+## About the project 
+This project is a landing page for a fashion company known as CatWalk Couture. 
+*Catwalk Couture is a fake company that I made up for the sole purpose of this project
 
-To get started, open `js/app.js` and start building out the app's functionality
+This project has HTML and CSS to style the website. It also has some JavaScript to make the website dynamic and interactive. 
 
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+## These are some of the features that this project has:
+
+* It has a navigation bar that lets the user dynamically navigate to different sections on the page by clickiing the nav buttions
+
+* It has a section that displays an about me section, a section for their work/what the company does, a portfolio section, and a section for a user to sign up to their newsletter 
+
+* The project also contains some icons and a few links to social media pages (they link to my social media pages but essentially it would be for the company's pages if the company were real), and a "back to top" button that when clicked, brings the user back to the top of the page. The back to the top button only appears once the user has scrolled farther down on the page, and when it appears, it is a button with a paw pointing upwards. 
--- js/app.js
+++ js/app.js
@@ -1,62 +1,118 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
+/// creating the array of portfolio item's individual text & image data
+// to be added in the project section
+const portfolioItems = [
+  {
+    name: "Leopard Print Is Purrrfect",
+    image: "/assets/fashionCatDefaultImage1.png",
+  },
+  {
+    name: "What Humans Don't Know",
+    image: "/assets/fashionCatDefaultImage2.png",
+  },
+  {
+    name: "Whiskers and Wardrobes",
+    image: "/assets/fashionCatDefaultImage1.png",
+  },
+  {
+    name: "From Alley Cat To Catwalk",
+    image: "/assets/fashionCatDefaultImage2.png",
+  },
+  {
+    name: "Tails and Trends",
+    image: "/assets/fashionCatDefaultImage1.png",
+  },
+  {
+    name: "How To Purrr-suade Your Human",
+    image: "/assets/fashionCatDefaultImage2.png",
+  },
+  {
+    name: "Paw Print: Why You Must Wear It",
+    image: "/assets/fashionCatDefaultImage1.png",
+  },
+  {
+    name: "How To Look Like a Cool Cat",
+    image: "/assets/fashionCatDefaultImage2.png",
+  },
+];
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
+// get nav list and all sections
+const navList = document.getElementById("navbar__list");
+const sections = document.querySelectorAll("section");
+// get the projectcontainer from project section to put portfolio items in
+const projectContainer = document.getElementById("projectContainer");
+// get the back to top button
+const backToTopButton = document.querySelector(".backToTopButton");
 
-/**
- * Define Global Variables
- * 
-*/
+// build the nav/adding the top nav buttons to the page
+// creating the nav items
+for (let i = 0; i < sections.length; i++) {
+  const section = sections[i];
+  const listItem = document.createElement("li");
+  listItem.innerHTML = `<a class="menu__link" href="#${section.id}">${section.dataset.nav}</a>`;
 
+  // add event listener to section/nav list buttons
+  listItem.addEventListener("click", function (e) {
+    e.preventDefault();
+    const targetId = e.target.getAttribute("href").substring(1);
+    const targetSection = document.getElementById(targetId);
+    targetSection.scrollIntoView({ behavior: "smooth" });
+  });
+  // appending nav list buttons/items to the navigation bar
+  navList.appendChild(listItem);
+}
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
+// logic to add active css classes to nav items if the user is in that section
+function makeActive() {
+  // logic to add active css classes to sections if user is in that section
+  for (const section of sections) {
+    const box = section.getBoundingClientRect();
+    // find value that works best
+    const value = 150; // >= -value
+    if (box.top <= value && box.bottom >= value) {
+      // apply active state on current section and corresponing nav link
+      section.classList.add("your-active-class");
+      document
+        .querySelector(`a[href="#${section.id}"]`)
+        .classList.add("active");
+    } else {
+      // remove active state from other section/nav link
+      section.classList.remove("your-active-class");
+      document
+        .querySelector(`a[href="#${section.id}"]`)
+        .classList.remove("active");
+    }
+  }
+  // logic for deciding if to show the back to top button or make it invisible
+  const rect = sections[0].getBoundingClientRect();
+  if (rect.top >= -5) {
+    // if user is below the top section, button will be invisible,
+    backToTopButton.classList.add("invisible");
+  }
+  // if user is above the top section, button will be visible
+  else {
+    backToTopButton.classList.remove("invisible");
+  }
 
+  
+}
 
+document.addEventListener("scroll", makeActive);
+//backToTopButton.classList.add("invisible");
+//  backToTopButton.classList.add("invisible");
+backToTopButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  const targetId = e.target.getAttribute("href").substring(1);
+  const targetSection = document.getElementById(targetId);
+  targetSection.scrollIntoView({ behavior: "smooth" });
+});
 
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
+// creating the the portfolio items in the project section
+for (let i = 0; i < 8; i++) {
+  const portfolioItem = document.createElement("div");
+  portfolioItem.innerHTML = `<div class="boxContainer">
+                      <img class="postImages" src="${portfolioItems[i].image}"></img>
+                      <div id="bottomBox"><h3>${portfolioItems[i].name}</h3>
+                      </div></div>`;
+  // add portfolio items to the project section/container
+  projectContainer.appendChild(portfolioItem);
+}
